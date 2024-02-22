c15b804f54c54b2674918ee6f0f7df1d1ead5bb0
raul <rgp5060rgmail.com>
2024-02-22 15:09:54 +0000

    tercer comit

--- Scripts/deploy.sh
+++ Scripts/deploy.sh
@@ -5,9 +5,9 @@ git clone https://github.com/Raulgp17/Practica-1-Tema6.git
 
 cd Biblioteca
 cd build
-mv classes ../src/main/webapp/WEB-INFsudo
+mv classes ../src/main/webapp/WEB-INF
 cd ..
-cd src/main/webapp
+cd src/master/webapp
 echo "admin" | sudo -S jar -cvf  /opt/tomcat/webapps/Biblioteca.war *
 sudo rm -rf /tmp/Biblioteca
 
